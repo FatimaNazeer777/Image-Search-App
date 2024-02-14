741adc6b348c260b9e232ca45b4c993e158c0cdd
Fatima Nazeer <147838099+FatimaNazeer777@users.noreply.github.com>
2024-02-14 11:45:11 +0500

    Add files via upload

---
+++ Image Search App/script.js
@@ -0,0 +1,54 @@
+const accessKey = "dUcGCPlJHOELBnSPRWEh4VQ_VFwqrG7ILrsjisXo7vI"
+
+const formElement = document.querySelector("form");
+const inputElement = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages (){
+    inputData = inputElement.value;
+    const url = `http://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch (url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1){
+        searchResults.innerHTML = ""
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+ });
+
+ page++;
+ if(page > 1) {
+    showMore.style.display = "block";
+ }
+ }
+
+ formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+ });
+
+ showMore.addEventListener("click", (event) => {
+    searchImages();
+ });
